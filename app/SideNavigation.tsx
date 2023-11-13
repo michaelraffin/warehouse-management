2bde7d3e2f674545eaccd3994d55fe4b47f1fea2
Michael <michaelraffinpaculba@gmail.com>
2023-11-13 17:22:57 +0800

    Cleaning

--- app/SideNavigation.tsx
+++ app/SideNavigation.tsx
@@ -183,14 +183,14 @@ className=' w-6 h-6'
                     </li>
                     <li>
                       <a
-                        href="/Paymongo"
+                        href="/staff"
                         className="group relative flex justify-center rounded px-2 py-1.5 text-gray-500 hover:bg-gray-50 hover:text-gray-700"
                       >
                       <img src="  https://static1.eyellowpages.ph/uploads/yp_business/photo/2066114/normal_paymongo-philippines-inc-1681184832.png"  className="w-6 h-8"/>
                             <span
                           className="absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white opacity-0 group-hover:opacity-100"
                         >
-                          Paymongo
+                          Staff
                         </span>
                       </a>
                     </li>
