a98a970f7a0134a3e5682f17281981167ee097c7
MichaelRaffinPaculba <michaelraffinpaculba@gmail.com>
2023-11-14 09:09:16 +0800

    Feature/2311/login supabase (#2)
    * Adding dynamic trycatch, supabase login
    
    * Adding Service Login,Token setter

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+"use client" 
+
+import React, { useEffect } from 'react';
 import Image from 'next/image'
 import { Button } from "@/components/ui/button"
 import Sheet from "@/app/local/sheet"
+import {signinAuth} from '../Utils/serviceLogin'
 export default function Home() {
+
+  const  signinAccount =()=>{
+    signinAuth().then(response=>{
+      console.log(response)
+          })
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -16,6 +26,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
+          
             By{' '}
             <Image
               src="/vercel.svg"
@@ -38,7 +49,9 @@ export default function Home() {
           priority
         />
       </div>
-
+      <Button onClick={signinAccount}>
+              Signin
+            </Button>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="/dashboard"
