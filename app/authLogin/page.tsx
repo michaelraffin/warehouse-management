fcaf474daa3666de85980e820862dcc88f09663f
Michael <michaelraffinpaculba@gmail.com>
2023-11-14 09:05:14 +0800

    Adding Service Login,Token setter

--- app/authLogin/page.tsx
+++ app/authLogin/page.tsx
@@ -1,18 +1,20 @@
 "use client" 
 import React, { useEffect } from 'react';
-
+import { useUrl } from 'nextjs-current-url';
 import { useRouter ,useSearchParams} from 'next/navigation'
 export default function Auth() {
-    const data = useRouter();
-    function Component() {
-        // { basePath: 'https://www.example.com/docs' }
-     }
+    const { href: currentUrl, pathname } = useUrl() ?? {};
+    const router = useRouter();
     useEffect(() => {
-        console.log(data)
-        // const urlSearchParams = new URLSearchParams(callbackUrl.split('?')[1]);
-        // const stateParam = urlSearchParams.get('state');
-        // const codeParam = urlSearchParams.get('code');
-        
+        const accessToken =  `${currentUrl}`
+        const access_token = new URLSearchParams(accessToken.split('#')[1]).get('access_token');
+        const provider_token = new URLSearchParams(accessToken.split('#')[1]).get('provider_token');
+        const refresh_token = new URLSearchParams(accessToken.split('#')[1]).get('refresh_token');
+        localStorage.clear()
+        localStorage.setItem('access_token', `${access_token}`);
+        localStorage.setItem('provider_token', `${provider_token}`);
+        localStorage.setItem('refresh_token', `${refresh_token}`);
+        router.push('/dashboard');
     })
 
     return (<>Loading..</>)
