4a5ef239851362fb86984c3c1a484210f72817e0
Michael <michaelraffinpaculba@gmail.com>
2024-11-24 11:13:39 +0800

    Adding details

--- app/SideNavigation.tsx
+++ app/SideNavigation.tsx
@@ -198,7 +198,7 @@ function SideNavigation() {
                     </li>
                     <li>
                       <a
-                        href="/staff"
+                        href="/dashboard"
                         className="group relative flex justify-center rounded px-2 py-1.5 text-gray-500 hover:bg-gray-50 hover:text-gray-700"
                       >
                         <img
