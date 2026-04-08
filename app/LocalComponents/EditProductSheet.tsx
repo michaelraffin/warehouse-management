4f1a67b3b010c2b4f1fbbd51f0ece2e0ebb825b4
niffar leahchim <winxraffin@gmail.com>
2026-04-09 00:09:48 +0800

    update product sheet edit

--- app/LocalComponents/EditProductSheet.tsx
+++ app/LocalComponents/EditProductSheet.tsx
@@ -96,16 +96,31 @@ export default function ProductDetailsSheet(props: any) {
     }
   };
 
+  function TagIcon({ size = 24, color = "#000", className = "" }) {
+    return (
+      <svg
+        fill={color}
+        width={size}
+        height={size}
+        viewBox="-2 0 19 19"
+        xmlns="http://www.w3.org/2000/svg"
+        className={className}
+      >
+        <path d="m13.842 11.52-4.389 4.388a1.112 1.112 0 0 1-1.567 0l-6.28-6.28a3.027 3.027 0 0 1-.771-1.892l.043-3.681A1.141 1.141 0 0 1 2 2.935L5.67 2.9a3.04 3.04 0 0 1 1.892.773l6.28 6.28a1.112 1.112 0 0 1 0 1.567zM3.826 5.133a.792.792 0 1 0-.792.792.792.792 0 0 0 .792-.792zm6.594 7.348a.554.554 0 0 0 0-.784l-.401-.401a2.53 2.53 0 0 0 .35-.83 1.565 1.565 0 0 0-.397-1.503 1.59 1.59 0 0 0-1.017-.46 2.14 2.14 0 0 0-.75.085h-.002a2.444 2.444 0 0 0-.59.277H7.61a2.677 2.677 0 0 0-.438.357 2.043 2.043 0 0 1-.259.22 1.29 1.29 0 0 1-.329.17h-.002a.835.835 0 0 1-.338.038h-.002a.53.53 0 0 1-.314-.136.539.539 0 0 1-.106-.534 1.54 1.54 0 0 1 .41-.71 1.632 1.632 0 0 1 .23-.165l.03-.019a1.783 1.783 0 0 1 .322-.155.942.942 0 0 1 .325-.06.554.554 0 0 0 0-1.108h-.001a2.058 2.058 0 0 0-.717.132 2.846 2.846 0 0 0-.529.26l-.01.006-.398-.4a.554.554 0 1 0-.784.785l.388.387a2.513 2.513 0 0 0-.347.803 1.644 1.644 0 0 0 .404 1.561 1.622 1.622 0 0 0 .983.456 1.922 1.922 0 0 0 .805-.089 2.372 2.372 0 0 0 .624-.319 3.142 3.142 0 0 0 .398-.339 1.569 1.569 0 0 1 .256-.208 1.381 1.381 0 0 1 .32-.151 1.023 1.023 0 0 1 .348-.038.485.485 0 0 1 .308.139c.05.049.165.165.097.488a1.558 1.558 0 0 1-.413.729 2.476 2.476 0 0 1-.28.219 1.727 1.727 0 0 1-.306.157.687.687 0 0 1-.32.042.554.554 0 1 0-.08 1.106c.052.004.103.005.152.005a1.723 1.723 0 0 0 .685-.134 2.678 2.678 0 0 0 .507-.27l.01-.007.397.398a.555.555 0 0 0 .783 0z" />
+      </svg>
+    );
+  }
   const formatter = new Intl.NumberFormat("en-PH", {
     style: "currency",
     currency: "PHP",
   });
+
   const editingContent = () => {
     if (isEditing) {
       return (
         <>
           <div className="flex items-center justify-between">
-            <p className="font-medium text-gray-700">Title</p>
+            <p className="font-medium text-gray-700 uppercase ">Title</p>
             <div className="flex space-x-2">
               <span className="font-light">
                 <input
@@ -117,27 +132,12 @@ export default function ProductDetailsSheet(props: any) {
               </span>
             </div>
           </div>
-          <div className="flex items-center justify-between">
-            <p className="font-medium text-gray-700">Stocks</p>
-            <Progress value={props.details.stocks} className="w-[60%] h-2" />
-          </div>
 
           <div className="flex items-center justify-between">
-            <p className="font-medium text-gray-700">Price</p>
-            <div className="flex space-x-2">
-              <span className="font-light">
-                <input
-                  value={price}
-                  placeholder={props.details.price}
-                  className="border-0 ring-2 outline-2 p-2 rounded-md"
-                  onChange={handleNumberChange}
-                  id={"Price"}
-                />
-              </span>
-            </div>
-          </div>
-          <div className="flex items-center justify-between">
-            <p className="font-medium text-gray-700">Bundle Price</p>
+            <p className="font-medium  text-gray-700 uppercase">
+              Price per Case/Box
+            </p>
+
             <div className="flex space-x-2">
               <span className="font-light">
                 <input
@@ -150,7 +150,25 @@ export default function ProductDetailsSheet(props: any) {
               </span>
             </div>
           </div>
+          <div className="flex items-center justify-between">
+            <p className="font-medium  text-gray-700 uppercase">price per PC</p>
+            <div className="flex space-x-2">
+              <span className="font-light">
+                <input
+                  value={price}
+                  placeholder={props.details.price}
+                  className="border-0 ring-2 outline-2 p-2 rounded-md"
+                  onChange={handleNumberChange}
+                  id={"Price"}
+                />
+              </span>
+            </div>
+          </div>
 
+          {/*<div className="flex items-center justify-between">
+            <p className="font-medium text-gray-700 uppercase">Stocks</p>
+            <Progress value={props.details.stocks} className="w-[60%] h-2" />
+          </div>*/}
           {/*<div className="flex items-center justify-between">
             <p className="font-medium text-gray-700">Liters</p>
             <div className="flex space-x-2">
@@ -170,7 +188,10 @@ export default function ProductDetailsSheet(props: any) {
 
           <Separator className="my-4" />
           {/* General Info */}
-          <div className="space-y-3">
+          <div
+            className="space-y-3"
+            style={{ display: !isEditing ? "none" : "block" }}
+          >
             <div className="grid grid-cols-2 gap-4 text-sm text-gray-500 mb-10"></div>
 
             <div className="mt-10 mb-20">
@@ -195,47 +216,46 @@ export default function ProductDetailsSheet(props: any) {
             </div>
           </div>
 
+          {/* Priority */}
+          {/*<div className="flex items-center justify-between">
+            <p className="font-medium text-gray-700">Priority</p>
+            <Badge variant="destructive" className="bg-red-500 text-white">
+              Critical
+            </Badge>
+          </div>*/}
+          {/* Status */}
+          {/*<div className="flex items-center justify-between">
+            <p className="font-medium text-gray-700">Status</p>
+            <Badge variant="outline" className="text-yellow-700 bg-yellow-200">
+              Pulled by company
+            </Badge>
+          </div>*/}
+
+          {/* Type */}
+          <div className="flex items-center justify-between">
+            <p className="font-medium text-gray-700">Price PC</p>
+            <p className="text-sm text-gray-500">
+              {formatter.format(props.details.price)}
+            </p>
+          </div>
+          <div className="flex items-center justify-between">
+            <p className="font-medium text-gray-700">Price Case/Box</p>
+            <p className="text-sm text-gray-500">
+              {formatter.format(props.details.bundlePrice ?? 0)}
+            </p>
+          </div>
+          {/*Stocks Badge*/}
           <div className="flex items-center justify-between">
             <p className="font-medium text-gray-700">
               Stocks: {props.details.stocks}
             </p>
             <Progress value={props.details.stocks} className="w-[60%] h-2" />
           </div>
-
-          <div className="flex items-center justify-between">
-            <p className="font-medium text-gray-700">Liters</p>
-            <div className="flex space-x-2">
-              <span className="font-light">
-                <p className="text-sm text-gray-500">{props.details.size}</p>
-              </span>
-            </div>
-          </div>
-          {/* Priority */}
-          <div className="flex items-center justify-between">
-            <p className="font-medium text-gray-700">Priority</p>
-            <Badge variant="destructive" className="bg-red-500 text-white">
-              Critical
-            </Badge>
-          </div>
-          {/* Status */}
-          <div className="flex items-center justify-between">
-            <p className="font-medium text-gray-700">Status</p>
-            <Badge variant="outline" className="text-yellow-700 bg-yellow-200">
-              Pulled by company
-            </Badge>
-          </div>
           {/* Date */}
           <div className="flex items-center justify-between">
-            <p className="font-medium text-gray-700">Date</p>
+            <p className="font-medium text-gray-700">Date Created</p>
             <p className="text-sm text-gray-500">
               {moment(props.details.dateAdded).format("MMM-DD-YYYY hh:mm A")}
-            </p>
-          </div>
-          {/* Type */}
-          <div className="flex items-center justify-between">
-            <p className="font-medium text-gray-700">Price</p>
-            <p className="text-sm text-gray-500">
-              {formatter.format(props.details.price)}
             </p>
           </div>
           <Separator className="my-4" />
@@ -277,12 +297,12 @@ export default function ProductDetailsSheet(props: any) {
               </div>
             </div>
           </div>
-          <div className="mt-10 mb-20">
+          {/*<div className="mt-10 mb-20">
             <Label htmlFor="picture" className="mt-10">
               Upload your photo
             </Label>
             <Input id="picture" type="file" />
-          </div>
+          </div>*/}
         </>
       );
     }
@@ -375,6 +395,7 @@ export default function ProductDetailsSheet(props: any) {
             <Button
               onClick={() => validateDetails()}
               variant="secondary"
+              style={{ visibility: !isEditing ? "hidden" : "visible" }}
               className="w-full rounded-full text-xs font-light mt-10"
             >
               Save details
